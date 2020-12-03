75807d6a88babde7b1af00c4ced82f4b90a34102
Alexander <alx925@gmail.com>
2020-12-03 13:03:49 +0300

    homework-6

---
+++ Урок 6. Компоненты Vue.js/js/errorComponent.js
@@ -0,0 +1,8 @@
+Vue.component('error', {
+   props: ['visibility'],
+   template: `
+      <div class="error-block" v-show="visibility">
+         <h4>Не удаётся выполнить запрос к серверу!</h4>
+      </div>
+   `
+});
